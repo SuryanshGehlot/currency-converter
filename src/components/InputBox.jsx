6084877c38ff50c7eb760631d8b8e49be1946c97
SuryanshGehlot <160301391+SuryanshGehlot@users.noreply.github.com>
2025-06-22 15:02:16 +0530

    Fix: allow input to be cleared by storing amount as string

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -8,7 +8,7 @@ function InputBox({ label, amount, onAmountChange, onCurrencyChange, currencyOpt
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
         <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">{label}</label>
-        <input id={amountInputId} className="outline-none w-full bg-transparent py-1.5" type="number" placeholder='Amount' disabled={amountDisable} value={amount} onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} />
+        <input id={amountInputId} className="outline-none w-full bg-transparent py-1.5" type="number" placeholder='Amount' disabled={amountDisable} value={amount} onChange={(e) => onAmountChange && onAmountChange(e.target.value)} />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className='text-black/40 mb-2 w-full'>Currency Type</p>
